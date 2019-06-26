9c3dd2a38954be0a2fa801849f5929ebd59035e3
olethomas93 <norbye93@gmail.com>
2019-06-26 14:32:10 +0200

    hello

--- rgb-matrix-app.js
+++ rgb-matrix-app.js
@@ -1,3 +1,4 @@
+
 var Matrix = require('rpi-matrix');
 var TextAnimation = require('../src/text-animation.js');
 var Animation = require('rpi-animations').Animation;
@@ -8,4 +9,7 @@ Matrix.configure({'led-cols':32, 'led-rows':32, 'led-chain':3,'led-parallel':2})
 
 var queue = new AnimationQueue();
 
-var A = new TextAnimation();
+var A = new TextAnimation({text:'dette er en test'});
+
+
+
